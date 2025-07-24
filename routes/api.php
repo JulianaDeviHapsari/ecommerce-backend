f57d8b667e818f846ec5ecc5a47c5863a51763df
Juliana Devi Hapsari <julianadevihapsari@gmail.com>
2025-07-24 16:51:40 +0700

    Finish : API Register ( Setup Auth + Register + Send Otp + Verify Register + Resend OTP + Login )
    Next :
    API Forgot Password

---
+++ routes/api.php
@@ -0,0 +1,16 @@
+<?php
+
+use App\Http\Controllers\AuthenticationController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+Route::post('/resend_otp', [AuthenticationController::class, 'resendOtp']);
+Route::post('/register', [AuthenticationController::class, 'register']);
+Route::post('/check-otp-register', [AuthenticationController::class, 'verifyOtp']);
+Route::post('/verify-register', [AuthenticationController::class, 'verifyRegister']);
+
+Route::post('/login', [AuthenticationController::class, 'login']);
